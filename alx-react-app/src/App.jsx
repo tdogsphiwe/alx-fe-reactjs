51a359b97140f3fc2679687fb4a211c83a8169f4
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-09 23:32:14 +0200

    added another componets

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <MainContent />
+      <Footer />
       <WelcomeMessage />
       <div>
         <a href="https://vitejs.dev" target="_blank">
