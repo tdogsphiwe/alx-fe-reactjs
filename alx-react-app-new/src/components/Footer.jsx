c7b0691ae57403a529ab3e5b64cea384253be388
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-15 20:36:31 +0200

    applied inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p style={{boder:'2px solid red' , color:'green'}}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+    <main>
+        <p>I <span style={{color:'pink'}}>love</span> to visit <span style={{color:'blue'}}>New York</span>, Paris, and Tokyo.</p>
+    </main>
+   );
+}
+
+export default MainContent;
