2c4de0ab50143053cd45d8a319a44c6f4946d378
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-15 21:26:28 +0200

    counter component added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,13 +7,14 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-
+import Counter from './components/counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Counter />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
