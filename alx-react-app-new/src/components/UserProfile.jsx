c25f9cfa26bdf87fbd298a2d9bf3acb892897afe
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-15 20:46:37 +0200

    i seriously cant find the error

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,11 +1,14 @@
 const UserProfile = (props) => {
-    return (
-      <div style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>
-        <h2 style={{ color: 'red' }}>{props.name}</h2>
-        <p style={{color:'green'}}>Age:<span style={{ fontWeight: 'bold' }}> {props.age}</span></p>
-        <p style={{fontWeight:'bold'}}>Bio: {props.bio}</p>
-      </div>
-    );
-  };
 
-  export default UserProfile;
+  return(
+      <div style={{ 
+          border: '1px solid gray',
+          padding: '10px', 
+          margin: '10px' }}>
+          <h2 style={{ color: 'blue' }}>Name: {props.name}</h2>
+          <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
+          <p style= {{color:'lightBlue'}}>Bio: {props.bio}</p>
+      </div>
+  )
+}
+export default UserProfile
