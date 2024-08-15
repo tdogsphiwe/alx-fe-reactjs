e260e0a37868c551d96aee4a7a2f6917b54086a9
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-15 20:39:53 +0200

    inline css style

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ const UserProfile = (props) => {
     return (
       <div style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>
         <h2 style={{ color: 'red' }}>{props.name}</h2>
-        <p>Age:<span style={{ fontWeight: 'bold' }}> {props.age}</span></p>
+        <p style={{color:'green'}}>Age:<span style={{ fontWeight: 'bold' }}> {props.age}</span></p>
         <p style={{fontWeight:'bold'}}>Bio: {props.bio}</p>
       </div>
     );
