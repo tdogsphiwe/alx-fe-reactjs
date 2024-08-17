f9b6d144e20aa22c5f32b1640e9339a201aada09
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-17 14:04:50 +0200

    Done

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Navbar(){
+
+  return (
+    <>
+
+        <nav style={{ padding: '10px', background: '#333',color: '#fff'}}>
+            <div>
+            <Link to= "/" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none'}}>Home</Link> 
+            <Link to="/about" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none'}}>About</Link> 
+            <Link to="/services"style={{ margin: '0 10px', color: '#fff', textDecoration: 'none'}}>Services</Link> 
+            <Link to="/contact"style={{ margin: '0 10px', color: '#fff', textDecoration: 'none'}}>Contact</Link> 
+            </div>
+        </nav> 
+
+    </>
+  );
+}
+
+export default Navbar;
