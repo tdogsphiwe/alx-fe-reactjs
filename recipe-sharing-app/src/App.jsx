3724340e92a7e1612db1aac2a568b0b03500c14e
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-22 01:21:39 +0200

    fixed an error

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import AddRecipeForm from './components/AddRecipeForm ';
+import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 
 function App() {
