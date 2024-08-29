acc07ac6fffd62a646e8f1705a8bbd6c15362f22
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-29 17:20:28 +0200

    modified

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -7,11 +7,28 @@ const RegistrationForm = () => {
 
     const handleSUbmit = (e) => {
         e.preventDefault();
-        if(usename && email && password){
-            console.log('Form SUbmitted' ,{ username, email, password});
-        }else{
-            console.log('Please fill in all fields');
-        }
+
+        let validationErrors = {};
+
+        if (!username) {
+            validationErrors.username = 'Username is required';
+          }
+      
+          if (!email) {
+            validationErrors.email = 'Email is required';
+          }
+      
+          if (!password) {
+            validationErrors.password = 'Password is required';
+          }
+      
+          setErrors(validationErrors);
+      
+          if (Object.keys(validationErrors).length === 0) {
+
+            console.log({ username, email, password });
+          }
+
     };
 
     return (
@@ -23,6 +40,7 @@ const RegistrationForm = () => {
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
+            {errors.username && <div style={{ color: 'red' }}>{errors.username}</div>} 
           </div>
           <div>
             <label>Email:</label>
@@ -31,6 +49,7 @@ const RegistrationForm = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
+               {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
           </div>
           <div>
             <label>Password:</label>
@@ -39,6 +58,7 @@ const RegistrationForm = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+             {errors.password && <div style={{ color: 'red' }}>{errors.password}</div>}
           </div>
           <button type="submit">Register</button>
         </form>
