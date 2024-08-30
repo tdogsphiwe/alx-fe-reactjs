dea453de37a643177bbbaa283a774231858ae9e9
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-08-31 00:07:59 +0200

    modifed

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ children, isAuthenticated }) {
+  let useAuth = { token: true };
   return isAuthenticated ? children : <Navigate to="/" />;
 }
 
