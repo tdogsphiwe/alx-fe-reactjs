bfd6eb35caf51fd902657fb2607c7ec89f66df57
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-09-22 18:41:09 +0200

    fixed an error

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,20 +1,11 @@
 import axios from 'axios';
 
-export const fetchAdvancedUserData = async ({ username, location, minRepos }) => {
-  try {
-    let query = `q=${username ? username : ''}`;
+export const fetchUserData = async (username, location, minRepos) => {
+  let query = '';
+  if (username) query += `user:${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
 
-    if (location) {
-      query += `+location:${location}`;
-    }
-
-    if (minRepos) {
-      query += `+repos:>${minRepos}`;
-    }
-
-    const response = await axios.get(`https://api.github.com/search/users?${query}`);
-    return response.data;
-  } catch (error) {
-    throw new Error('Error fetching user data');
-  }
+  const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+  return response.data;
 };
