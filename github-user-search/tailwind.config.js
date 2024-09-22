bb75790aaae88cd746512569742ab072206b8528
Sphiwe Lubisi <tdogsphiwe43@gmail.com>
2024-09-22 18:34:29 +0200

    installed tailwind

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
